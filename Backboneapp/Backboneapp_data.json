6b005e2ae297b5be3bf00d47c85c3e2ce97293ac
L0stSoul <LostSoul@beyondhteclouds.net>
2011-10-28 09:59:30 +0400

    Added backbone sample app

---
+++ Backboneapp/Backboneapp_data.json
@@ -0,0 +1,32 @@
+﻿[	
+	{
+		"position": 1,
+		"title": "It's backbone!",
+		"content": "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec leo sed urna varius lobortis vitae vitae elit. Praesent dictum odio nec dui facilisis non vehicula neque mattis. Vivamus orci turpis, viverra ac molestie ut, tincidunt ac massa. Vestibulum dignissim pellentesque enim non hendrerit. Nunc ultrices mi ante, at aliquam sapien. Maecenas dictum blandit est, eu venenatis elit laoreet at. Etiam at purus tellus, ac consectetur orci. Sed quam mi, dignissim convallis vehicula nec, mollis et sapien. Nunc feugiat consectetur urna quis vestibulum. Fusce consequat felis id dolor porta vitae pretium nisl pretium. Aliquam dictum eros vel eros condimentum euismod. Phasellus vitae mauris quam, cursus commodo nisi. Suspendisse suscipit orci sem. Fusce tempus suscipit dolor, eu porta augue consectetur vel. Proin enim nulla, varius quis vulputate consectetur, luctus et erat.</p>"
+	},
+	{
+		"position": 2,
+		"title": "It's really work!",
+		"content": "<p> for Real!</p>"
+	},
+	{
+		"position": 3,
+		"title": "you can click here",
+		"content": "<p> yeah! right!</p>"
+	},
+	{
+		"position": 4,
+		"title": "or even here!",
+		"content": "<p> oh yeah! i'm invincible</p>"
+	},
+	{
+		"position": 5,
+		"title": "nobody can stop you!",
+		"content": "<p> right! they can't!</p>"
+	},
+	{
+		"position": 6,
+		"title": "It's backbone!",
+		"content": "<p> Another big ipsum text here: ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec leo sed urna varius lobortis vitae vitae elit. Praesent dictum odio nec dui facilisis non vehicula neque mattis. Vivamus orci turpis, viverra ac molestie ut, tincidunt ac massa. Vestibulum dignissim pellentesque enim non hendrerit. Nunc ultrices mi ante, at aliquam sapien. Maecenas dictum blandit est, eu venenatis elit laoreet at. Etiam at purus tellus, ac consectetur orci. Sed quam mi, dignissim convallis vehicula nec, mollis et sapien. Nunc feugiat consectetur urna quis vestibulum. Fusce consequat felis id dolor porta vitae pretium nisl pretium. Aliquam dictum eros vel eros condimentum euismod. Phasellus vitae mauris quam, cursus commodo nisi. Suspendisse suscipit orci sem. Fusce tempus suscipit dolor, eu porta augue consectetur vel. Proin enim nulla, varius quis vulputate consectetur, luctus et erat.</p>"
+	}
+]
---
+++ Backboneapp/Scripts/Backboneapp.js
@@ -0,0 +1,91 @@
+﻿/// <reference path="Shared/underscore.js" />
+/// <reference path="Shared/backbone.js" />
+/// <reference path="Shared/jquery-1.6.2.min.js" />
+/// <reference path="Shared/Jquery.tmpl.min.js" />
+
+$(document).ready(function ()
+{
+    var Item = Backbone.Model.extend({
+    });
+
+    var ItemCollection = Backbone.Collection.extend({
+        model: Item,
+        comparator: function (item)
+        {
+            return item.get("position");
+        }
+    });
+
+    var MenuView = Backbone.View.extend({
+        el: $('#backbone-menu'),
+        template: $('#item-tmpl').template(),
+
+        render: function ()
+        {
+            this.el.empty();
+            $.tmpl(this.template, this.model.toArray()).appendTo(this.el);
+            return this;
+        }
+    });
+
+    var ContentView = Backbone.View.extend({
+        el: $('#backbone-content'),
+        template: $('#content-tmpl').template(),
+
+        render: function ()
+        {
+            this.el.empty();
+            $.tmpl(this.template, this.model).appendTo(this.el);
+
+            return this;
+        }
+    });
+
+
+    var NavigationRouter = Backbone.Router.extend({
+        _data: null,
+        _items: null,
+        _view: null,
+
+        routes: {
+            "info/:id": "showInfo",
+            "*actions": "defaultRoute"
+        },
+        initialize: function (options)
+        {
+            var _this = this;
+            $.ajax({
+                url: "Backboneapp_data.json",
+                dataType: 'json',
+                data: {},
+                async: false,
+                success: function (data)
+                {
+                    _this._data = data;
+                    _this._items = new ItemCollection(data);
+                    _this._view = new MenuView({ model: _this._items });
+                    _this._view.render();
+                    Backbone.history.loadUrl();
+                }
+
+            });
+
+            return this;
+        },
+        defaultRoute: function (actions)
+        {
+            this.showInfo(1);
+        },
+        showInfo: function (id)
+        {
+            var view = new ContentView({ model: this._items.at(id - 1) });
+            $(".active").removeClass("active");
+            $("#item" + id).addClass("active");
+            view.render();
+        }
+    });
+
+    var navigationRouter = new NavigationRouter;
+    Backbone.history.start();
+});
+
