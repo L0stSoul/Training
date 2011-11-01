52838f3f943c85e4bc6b03a557708eb769eb0167
L0stSoul <LostSoul@beyondhteclouds.net>
2011-11-01 20:52:08 +0300

    -

--- jsInheritance/Scripts/Inheritance.js
+++ jsInheritance/Scripts/Inheritance.js
@@ -7,23 +7,40 @@
     Child.prototype.constructor = Child;
     Child.super = Parent.prototype;
 }
-
+/*
 function inherit_B( Child, Parent )
 {
     for (var prop in Parent.prototype)
        if( !Child.prototype[prop] ) Child.prototype[prop] = Parent.prototype[prop];
     Child.prototype.super = Parent.prototype;
 }
+*/
+function inherit_B(Child, Parent)
+{
+    var F = function () { };
+    F.prototype = Parent.prototype;
+    var f = new F();
+    
+    for (var prop in Child.prototype) f[prop] = Child.prototype[prop];
+    Child.prototype = f;
+    Child.prototype.super = Parent.prototype;
+}
+
 function inherit_C(Child, Parent)
 {
-    for (var prop in Parent.prototype)
-        if (!Child.prototype[prop]) Child.prototype[prop] = Parent.prototype[prop];
+    var F = function () { };
+    F.prototype = Parent.prototype;
+    var f = new F();
+
+    for (var prop in Child.prototype) f[prop] = Child.prototype[prop];
+    Child.prototype = f;
     Child.prototype[Parent.prototype.__class_name] = Parent.prototype;
 }
 
 
 function inhertit_multiple(child)
 {
+    
     for( var i = 1; i < arguments.length; ++i )
     {
        var parent = arguments[i]
