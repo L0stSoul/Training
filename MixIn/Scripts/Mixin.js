54f358952378de2cc7d31a23abd13dae656c21f5
L0stSoul <LostSoul@beyondhteclouds.net>
2011-11-09 21:31:27 +0300

    -mixin extend_2 added

--- MixIn/Scripts/Mixin.js
+++ MixIn/Scripts/Mixin.js
@@ -12,3 +12,23 @@
         }
     }
 }
+
+function extend_2(object)
+{
+    var mixins = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < mixins.length; ++i)
+    {
+        for (var prop in mixins[i])
+        {
+            if (typeof object.prototype[prop] === "undefined")
+            {
+                bindMethod = function (mixin, prop)
+                {
+                    return function () { mixin[prop].apply(this, arguments) }
+                }
+
+                object.prototype[prop] = bindMethod(mixins[i], prop);
+            }
+        }
+    }
+}
